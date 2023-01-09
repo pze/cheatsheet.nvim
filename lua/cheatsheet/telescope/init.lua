728ba3c48b9b5185b788037a12e60bb3c4b8c62e
towry <towry@users.noreply.github.com>
2023-01-09 11:06:06 +0800

    use previewer

--- lua/cheatsheet/telescope/init.lua
+++ lua/cheatsheet/telescope/init.lua
@@ -1,19 +1,20 @@
-local actions = require('telescope.actions')
-local actions_state = require('telescope.actions.state')
+-- local actions = require('telescope.actions')
+-- local actions_state = require('telescope.actions.state')
 local finders = require('telescope.finders')
+local previewers = require('telescope.previewers')
 local pickers = require('telescope.pickers')
 
 local config = require('telescope.config').values
 local entry_display = require('telescope.pickers.entry_display')
 
 local cheatsheet = require('cheatsheet')
-local utils = require('cheatsheet.utils')
+-- local utils = require('cheatsheet.utils')
 
 local M = {}
 
 -- Filter through cheats using Telescope
 -- Highlight groups:
---     cheatMetadataSection, cheatDescription, cheatCode
+--     cheatMetadataSection, cheatDescription, cheatcode
 -- Mappings:
 --     <C-E> - Edit user cheatsheet in new buffer
 --     <C-Y> - Yank the cheatcode
@@ -26,33 +27,6 @@ M.pick_cheat = function(telescope_opts, opts)
     finder = finders.new_table {
       results = cheatsheet.get_cheats(opts),
       entry_maker = function(entry)
-        -- Calculate the width of each column dynamically so that both
-        -- the description and cheatcode is readable on small terminals too.
-        -- This whole logic can be avoided if the cheatcode is shown first and
-        -- a small width for the respective cheatcode column is used.
-        -- But the cheatcode is what we *don't* know and the description is
-        -- what we already know. So show description first for better UX.
-
-        -- * config.width was deprecated in favor of config.layout_config.width
-        -- https://github.com/nvim-telescope/telescope.nvim/commit/5a53ec5c2fdab10ca8775d3979b1a85e63d57953
-        -- * config.layout_config was changed to move width and height to individual layout_strategy configs
-        -- https://github.com/nvim-telescope/telescope.nvim/pull/1039/files#diff-4936325bfc521d7cffc09fe0156becd4a4ba2ed169431c94bd63669fe0cc1a2aL79-L80
-        local cols = vim.o.columns
-        local width = config.width
-            or (config.layout_config and config.layout_config.width)
-            or
-            (
-            config.layout_config and config.layout_config[config.layout_strategy] and
-                config.layout_config[config.layout_strategy].width)
-            or cols
-        local tel_win_width
-        -- width = 80 -> column width, width = 0.7 -> ratio
-        if width > 1 then
-          tel_win_width = width
-        else
-          tel_win_width = math.floor(cols * width)
-        end
-        local cheatcode_width = math.floor(cols * 0.55)
         local section_width = 10
 
         -- NOTE: the width calculating logic is not exact, but approx enough
@@ -61,10 +35,8 @@ M.pick_cheat = function(telescope_opts, opts)
           items = {
             { width = section_width }, -- section
             {
-              width = tel_win_width - cheatcode_width
-                  - section_width,
+              remaining = true,
             }, -- description
-            { remaining = true }, -- cheatcode
           },
         }
 
@@ -73,7 +45,6 @@ M.pick_cheat = function(telescope_opts, opts)
             -- text, highlight group
             { ent.value.section, "cheatMetadataSection" },
             { ent.value.description, "cheatDescription" },
-            { ent.value.cheatcode, "cheatCode" },
           }
         end
 
@@ -91,6 +62,17 @@ M.pick_cheat = function(telescope_opts, opts)
         }
       end,
     },
+    previewer = previewers.new({
+      preview_fn = function(_, entry, status)
+        local preview_win = status.preview_win
+        local bufnr = vim.api.nvim_win_get_buf(preview_win)
+        local text = { entry.value.cheatcode or "no code" }
+        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, text)
+      end,
+      title = function(_)
+        return "Code"
+      end,
+    }),
     attach_mappings = function(prompt_bufnr, map)
       local mappings = require('cheatsheet.config').options.telescope_mappings
       for keybind, action in pairs(mappings) do
